47835131ca4b2cc1dc59f51aba29e3d1d902a2a0
Guilherme <guilhermematt16@gmail.com>
2026-03-21 17:05:41 -0300

    Refactor pipeline to remove boto3 session usage

--- scripts/pipeline_silver_churn.py
+++ scripts/pipeline_silver_churn.py
@@ -1,34 +1,31 @@
-import boto3
 import awswrangler as wr
 import pandas as pd
 
 
-session = boto3.Session()
-path_s3 = "s3://projetochurnaws/Bronze/WA_Fn-UseC_-Telco-Customer-Churn.csv"
-path_silver = "s3://projetochurnaws/Silver/churn_refinado.parquet"
+path_bronze = 's3://projetochurnaws/Bronze/WA_Fn-UseC_-Telco-Customer-Churn.csv'
+path_silver = 's3://projetochurnaws/Silver/churn_refinado.parquet'
 
 
-df = wr.s3.read_csv(path=path_s3, boto3_session=session)
+df = wr.s3.read_csv(path=path_bronze)
 
 
-df = df.drop_duplicates(subset=['customerID'])
-df = df.dropna(subset=['MonthlyCharges', 'tenure'])
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+df = df.dropna(subset=['TotalCharges'])
 
 
-colunas_servicos = ['PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 
-                    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+colunas_servicos = [
+    'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 
+    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies'
+]
 
 df['Qtd_Servicos'] = (df[colunas_servicos] == 'Yes').sum(axis=1)
+
+
 df['TotalCharges_Pipeline'] = (df['MonthlyCharges'] * df['tenure']).round(2)
-
-
-df = df.drop(columns=['customerID'])
 
 
 wr.s3.to_parquet(
     df=df,
     path=path_silver,
     dataset=True,
-    mode="overwrite",
-    boto3_session=session
-)
+    mode="
