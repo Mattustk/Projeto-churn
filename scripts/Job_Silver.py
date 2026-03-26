c92280ec5c9fc254af2c4c38e8f49e2c4a347056
Guilherme <guilhermematt16@gmail.com>
2026-03-26 14:02:57 -0300

    Update Job_Silver.py

--- scripts/Job_Silver.py
+++ scripts/Job_Silver.py
@@ -1,32 +1,35 @@
-import boto3
+# Importação de Blibiotecas
 import awswrangler as wr
 import pandas as pd
 
-
+#Salvando o caminho do bucket S3 em path
 path_bronze = 's3://projetochurnaws/Bronze/WA_Fn-UseC_-Telco-Customer-Churn.csv'
 path_silver = 's3://projetochurnaws/Silver/churn_refinado.parquet'
 
-
+#Criação do dataFrame usando a camada Bronze do S3
 df = wr.s3.read_csv(path=path_bronze)
 
 
-df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-df = df.dropna(subset=['TotalCharges'])
+df = df.drop_duplicates(subset=['customerID']) #Impede que entre duplicatas no dataset           
+df = df.dropna(subset=['MonthlyCharges','tenure']) #Deleta valores inválidos de entrarem no DataSet
 
+df = df.drop(columns=['TotalCharges']) #Apaguei a tabela TotalCharges por estar com inconsistências na tipagem
 
 colunas_servicos = [
     'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 
-    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies'
-]
+    'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies' ] #criação da tabela de serviços para a criação de uma nova tabela no DF
 
-df['Qtd_Servicos'] = (df[colunas_servicos] == 'Yes').sum(axis=1)
+df['Qtd_Servicos'] = (df[colunas_servicos] == 'Yes').sum(axis=1) #usei o "axis =1" para somar realizada no eixo das colunas (horizontal) para contabilizar serviços por clientes
 
 
-df['TotalCharges_Pipeline'] = (df['MonthlyCharges'] * df['tenure']).round(2)
+df['TotalCharges_Pipeline'] = (df['MonthlyCharges'] * df['tenure']).round(2) #Criação da nova tabela de Total Charges so que com a tipagem certa
+df = df.dropna(subset=['TotalCharges_Pipeline']) #Filtro para eliminar valores inválidos
+
 
 
 wr.s3.to_parquet(
     df=df,
     path=path_silver,
     dataset=True,
-    mode="
+    mode="overwrite", # Modo Overwrite vai impedir que fique arquivo duplicado no S3
+)
